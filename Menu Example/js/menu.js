9da148488fc26ae6e25b172831b3e2f0f880e669
bethanyjames95 <bethanyj333@gmail.com>
2021-01-28 08:24:35 -0800

    update

--- Menu Example/js/menu.js
+++ Menu Example/js/menu.js
@@ -5,5 +5,7 @@ window:addEventListener('load', ()=>{
 
     hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
 
+    /* takes care of removing responsive if it gets turned on. We can run into problems with things. */
+    window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove('responsive');};
 });
 
