467c642f3b567d76cbb25020890372a5779a1abc
bethanyjames95 <bethanyj333@gmail.com>
2021-01-28 21:12:27 -0800

    menu bar

--- Lesson04/js/menu.js
+++ Lesson04/js/menu.js
@@ -1,7 +1,7 @@
 
 window:addEventListener('load', ()=>{
-    const hambutton = document.querySelector('.menu-button');
-    const mainnav = document.querySelector('.navigation');
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
 
     hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
 
