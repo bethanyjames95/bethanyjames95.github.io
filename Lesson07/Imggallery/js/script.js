66bbb2398a7c558d0ae71a64632eb57f52916bf3
bethanyjames95 <bethanyj333@gmail.com>
2021-02-21 00:55:18 -0800

    update

--- Lesson07/Imggallery/js/script.js
+++ Lesson07/Imggallery/js/script.js
@@ -59,42 +59,40 @@ WebFont.load({
     }
 }); 
 
-
-//Lazy Loading
-// says after page has loaded
-window.addEventListener("load",(event)=>{
+        // says after page has loaded
+        window.addEventListener("load",(event)=>{
             
-    const images = document.querySelectorAll("[data-src]");
-    //console.log(images);
+            const images = document.querySelectorAll("[data-src]");
+            //console.log(images);
 
-    function preloadImage(img){
-        console.log("trying to load"+img);
-        const src= img.getAttribute("data-src");
-        if(!src){
-            return;
-        }
-        img.src = src;
-    }
-    const imgOptions = {
-        threshold:0,
-        rootMargin: "0px 0px 200px 0px"
-    }
-
-    const imgObserver = new IntersectionObserver( (entries, imgObserver)=> {
-        console.log(entries);
-        entries.forEach(entry=>{
-            if(!entry.isIntersecting){
-                return;
+            function preloadImage(img){
+                console.log("trying to load"+img);
+                const src= img.getAttribute("data-src");
+                if(!src){
+                    return;
+                }
+                img.src = src;
             }
-            else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
+            const imgOptions = {
+                threshold:0,
+                rootMargin: "0px 0px 200px 0px"
             }
-        })
 
-    }, imgOptions);
-    images.forEach(image=>{
-        imgObserver.observe(image);
-    });
+            const imgObserver = new IntersectionObserver( (entries, imgObserver)=> {
+                console.log(entries);
+                entries.forEach(entry=>{
+                    if(!entry.isIntersecting){
+                    return;
+                    }
+                    else {
+                    preloadImage(entry.target);
+                    imgObserver.unobserve(entry.target);
+                    }
+                })
+
+            }, imgOptions);
+            images.forEach(image=>{
+                imgObserver.observe(image);
+            });
     
-});
+        });
